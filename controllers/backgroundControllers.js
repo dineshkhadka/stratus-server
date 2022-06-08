ea9b67cf26ee0428ce7941f992de761a12d2266b
Dinesh Khadka <dineshlakai@gmail.com>
2022-06-08 19:41:05 +0545

    Increased collections fetched images

--- controllers/backgroundControllers.js
+++ controllers/backgroundControllers.js
@@ -5,7 +5,7 @@ const Router = express.Router();
 
 var API_URL = `https://api.unsplash.com`;
 var API_KEY = `b26f90cbe29ca389bf76ad3176e29bda81ef9fc80e6792ce828664660ca3afdc`;
-var collection = `${API_URL}/collections/26321157/photos?client_id=${API_KEY}`;
+var collection = `${API_URL}/collections/26321157/photos?per_page=100&client_id=${API_KEY}`;
 
 Router.get('/', async (req, res) => {
     axios.get(collection)
