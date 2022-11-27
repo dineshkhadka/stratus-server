07dc9ea8864de4bf60e0482031c889e8e254fc82
Dinesh Khadka <dineshlakai@gmail.com>
2022-11-27 08:40:55 +0545

    Fixed issue with pagination

--- controllers/backgroundControllers.js
+++ controllers/backgroundControllers.js
@@ -1,22 +1,30 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const Router = express.Router();
-require('dotenv').config()
+require("dotenv").config();
 
-var API_URL = `https://api.unsplash.com`;
-var API_KEY = process.env.UNSPLASH_APIKEY;
-console.log(process)
-var collection = `${API_URL}/collections/26321157/photos?per_page=100&client_id=${API_KEY}`;
+const API_URL = `https://api.unsplash.com`;
+const API_KEY = process.env.UNSPLASH_APIKEY;
+const PER_PAGE = 10;
+const collection = `${API_URL}/collections/26321157/?client_id=${API_KEY}`;
 
-Router.get('/', async (req, res) => {
-    axios.get(collection)
-        .then(function (response) {
-            res.send(response.data)
-        })
-        .catch(function (error) {
-            console.log(error);
-            res.send(500)
-        })
-})
+function randomPage(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+Router.get("/", async (req, res) => {
+  const collectionData = await axios.get(collection);
+  await axios
+    .get(
+      `${API_URL}/collections/26321157/photos?page=${randomPage(1, Math.ceil(collectionData.data.total_photos / PER_PAGE))}&per_page=${PER_PAGE}&client_id=${API_KEY}`
+    )
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.send(500);
+    });
+});
 
 module.exports = Router;
